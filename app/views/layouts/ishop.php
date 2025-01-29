44eddca0d1146380010c94aaabcd80c564706104
Nikolazzzer <nikola-tarasov@mail.ru>
2025-01-29 18:21:39 +0300

    first commit

---
+++ app/views/layouts/ishop.php
@@ -0,0 +1,18 @@
+<?php
+
+use wfm\View;
+
+/** @var $this View */
+
+?>
+
+<?php $this->getPart('parts/header');?>
+
+<?php echo $this->content?>
+
+<?php $this->getPart('parts/footer'); ?>
+
+
+
+
+
---
+++ config/config_db.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'dsn' => 'mysql:host=MySQL-8.2;dbname=newishop;charset=utf8',
+    'user' => 'root',
+    'password' => '',
+];
